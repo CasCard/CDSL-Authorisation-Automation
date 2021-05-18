e20c920c945f298ea01c874aca0b7f01630c8d94
Abel C Dixon <abelcheruvathoor@gmail.com>
2021-05-18 14:09:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+DIRECTORY=os.getcwd()
+
+GMAIL_USERNAME="Gmail username"
+GMAIL_PASSWORD="Gmail password"
+
+KITE_USERNAME="Kite Username"
+KITE_PASSWORD="Kite Password"
+KITE_PIN="Kite PIN"
+
+CDSL_PIN="CDSL PIN"
+
+WEB_DRIVER_LOCATION=f"{DIRECTORY}\chromedriver.exe"
