5cbacd4689c44d3539c954b4650ab76254a17db2
Abel C Dixon <abelcheruvathoor@gmail.com>
2021-05-19 12:18:39 +0530

    bug fix and improvements

--- authorisation.py
+++ authorisation.py
@@ -1,3 +1,5 @@
+# Author : Abel C Dixon
+
 from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,38 +13,54 @@ import email.utils
 import bs4 as bs
 from bs4 import NavigableString, Tag
 import re
+
+# to store the from address email
+
 from_address=""
+message=""
+OTP=0
 
 KITE_URL="https://kite.zerodha.com/"
 HOLDINGS_URL="https://kite.zerodha.com/holdings"
 
-message=""
-OTP=0
+
+# pattern for extracting time from email
 pattern='(\d{2}):(\d{2}):(\d{2})'
 
 option=Options()
 
 driver=webdriver.Chrome(executable_path=config.WEB_DRIVER_LOCATION)
+option.add_experimental_option("prefs",{"profile.default_content_setting_values.notifications": 2})
 
-option.add_experimental_option("prefs",
-{"profile.default_content_setting_values.notifications": 2
- })
+# navigating to kite url
 driver.get(KITE_URL)
+
+#  filling the user id and password
 driver.find_element_by_id('userid').send_keys(config.KITE_USERNAME)
 driver.find_element_by_id('password').send_keys(config.KITE_PASSWORD)
+
+# clicking login button
 driver.find_element_by_class_name("button-orange").click()
 driver.implicitly_wait(60)
+
+# entering security pin
 driver.find_element_by_id('pin').send_keys(config.KITE_PIN)
 driver.find_element_by_class_name("button-orange").click()
 driver.implicitly_wait(60)
 time.sleep(2)
+
+# navigating to holding page
 driver.get(HOLDINGS_URL)
 driver.implicitly_wait(60)
 time.sleep(2)
+
+# selecting "Authorisation" option
 driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/section/div/div/div/span[2]/a[1]").click()
 time.sleep(2)
 driver.implicitly_wait(60)
-windows_before=driver.window_handles[0]
+kite_window=driver.window_handles[0]
+
+# Selecting "Continue" in authorisation pop up
 try:
     WebDriverWait(driver,8).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div/button[1]")))
     driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div/button[1]").click()
@@ -50,34 +68,42 @@ except TimeoutException:
     print("Page not loaded")
 time.sleep(2)
 driver.implicitly_wait(60)
-windows_after=driver.window_handles[1]
-driver.switch_to.window(windows_after)
+
+# Switching to CDSL page
+cdsl_window=driver.window_handles[1]
+driver.switch_to.window(cdsl_window)
 driver.implicitly_wait(60)
 time.sleep(3)
+
+# Selecting "Continue to CDSL"
 driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/button").click()
 time.sleep(3)
+
+# Entering TPIN
 driver.find_element_by_id("txtPIN").send_keys(config.CDSL_PIN)
 driver.find_element_by_id("btnCommit").click()
 driver.implicitly_wait(60)
 
+# Calculating the current system time and converting to seconds
 current_time=time.localtime()
+
 current_minute=int(time.strftime("%M", current_time))
 current_second=int(time.strftime("%S", current_time))
 total_seconds=(current_minute*60)+current_second
-print("Execution time")
-print(current_time,total_seconds)
-recieved=False
-driver.switch_to.window(windows_before)
+
+total_seconds=total_seconds-5
+
+# connecting to imap gor gmail access
 server=e.connect("imap.gmail.com", config.GMAIL_USERNAME, config.GMAIL_PASSWORD)
+
 while from_address != "edis@cdslindia.co.in":
     message=server.listup(1)[0]
-    print(message)
     time_of_new_email=message.date
     match = re.search(pattern, str(time_of_new_email))
     new_email_second=(int(match.group()[3:5])*60)+int(match.group()[6:9])
-    print(new_email_second)
+
+    # Checking for latest email from the CDSL
     if new_email_second>total_seconds:
-        print(match.group())
         # message = server.mail(server.listids()[0])
         from_address=email.utils.parseaddr(message.from_addr)[1]
         print(from_address)
@@ -85,7 +111,7 @@ while from_address != "edis@cdslindia.co.in":
 
 print(message)
 
-# MINUTE = time.strftime('%H:%M:%S')
+# Extracting OTP from email
 
 soup=bs.BeautifulSoup(message.body,'lxml')
 for br in soup.findAll('br'):
@@ -100,13 +126,13 @@ for br in soup.findAll('br'):
             break
 print(OTP)
 driver.implicitly_wait(60)
-driver.switch_to.window(windows_after)
+driver.switch_to.window(cdsl_window)
 driver.implicitly_wait(60)
 driver.find_element_by_id("OTP").send_keys(OTP)
 driver.implicitly_wait(60)
 driver.find_element_by_id("VerifyOTP").click()
 driver.implicitly_wait(60)
-time.sleep(5)
+time.sleep(3)
 print("Success")
 driver.quit()
 
